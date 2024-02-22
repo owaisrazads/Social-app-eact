13a8b5f0ec07a7b13922efe8415c74eaba25d1d2
unknown <oraza7969@gmail.com>
2024-02-22 02:25:42 -0800

    complete navbar components

--- social react app/src/components/Navbar/UserLinks.jsx
+++ social react app/src/components/Navbar/UserLinks.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {  Tooltip } from "@material-tailwind/react";
+import { Avatar } from "@material-tailwind/react";
+import avatar from "../../assets/images/avatar.png"
 
 const UserLinks = () => {
   return (
@@ -11,6 +14,12 @@ const UserLinks = () => {
       </div>
       <div  className='hover:translate-y-1 duration-500 ease-in-out hover:text-blue-500'>
       <i class="fa-regular fa-bell text-xl ml-3"></i>
+      </div>
+      <div className='mx-4 flex items-center'>
+        <Tooltip content="Sign Out" placement= "bottom" >
+        <Avatar src= {avatar} size='sm' alt='avatar' variant='rounded' ></Avatar>
+        </Tooltip>
+        <p className='ml-4 font-roboto text-sm text-black font-medium no-underline'>User</p>
       </div>
     </div>
   )
