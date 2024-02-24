8ff1b568067519c805e4e8bc10e68ae86cf6e063
unknown <oraza7969@gmail.com>
2024-02-24 06:19:35 -0800

    side bar

---
+++ social react app/src/components/LeftSideBar/leftSide.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const leftSide = () => {
+  return (
+    <div>
+      leftSide
+    </div>
+  )
+}
+
+export default leftSide
--- social react app/src/components/Pages/Home.jsx
+++ social react app/src/components/Pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import leftSide from '../LeftSideBar/leftSide'
 
 const Home = () => {
   return (
-    <div>
+    <div className='w-full' >
+      <div className='fixed top-0 z-10 w-full bg-white'>
       <Navbar/>
+      </div>
+      <div className='flex bg-gray-400'>
+        <div>
+        <leftSide/>
+        </div>
+      </div>
     </div>
   )
 }
