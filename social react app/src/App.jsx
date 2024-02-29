50a78103e8154b330bc029b2b43ce7f377246ef1
M Owais <136558431+owaisrazads@users.noreply.github.com>
2024-02-29 23:27:02 +0500

    Update App.jsx

--- social react app/src/App.jsx
+++ social react app/src/App.jsx
@@ -1,6 +1,8 @@
 import Pages from "./components/Pages/Pages";
 // import pages 
 
+// function
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline text-center">
